f1bee8bdb03df3248f25a6843ab59c92a6f80165
Thien Trieu <thien.trieu@telus.com>
2022-11-28 14:18:59 -0800

    added POST route to recieve form submission.

--- express_server.js
+++ express_server.js
@@ -34,6 +34,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
