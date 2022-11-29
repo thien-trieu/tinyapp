3ddb1bd1b2d7dc525108b05c82b49d978a5a3c63
Thien Trieu <thien.trieu@telus.com>
2022-11-28 18:18:32 -0800

    short URL to longURL

--- express_server.js
+++ express_server.js
@@ -58,11 +58,14 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortId}`); 
 });
 
-
-
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
